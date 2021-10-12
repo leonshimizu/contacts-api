b10624c668191d52e0209ccea1a10f8eb8e175f4
Leon <lmshimizu@gmail.com>
2021-10-11 18:01:09 -0700

    tested new Contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,20 +1,2 @@
-class Contact
-
-  attr_accessor :first_name, :last_name, :email, :phone_number
-
-  def initialize(input_options)
-    @first_name = input_options[:first_name]
-    @last_name = input_options[:last_name]
-    @email = input_options[:email]
-    @phone_number = input_options[:phone_number]
-  end
-
-  def full_name
-    "#{first_name} #{last_name}"
-  end
-
-  def info
-    "Hi my name is #{full_name} and you can get ahold of me by email (#{@email}) or by phone (#{@phone_number})."
-  end
-
+class Contact < ApplicationRecord
 end
