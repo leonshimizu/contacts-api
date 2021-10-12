01fc157fcb3cfa7848df6582b5eb2001ec61576e
Leon <lmshimizu@gmail.com>
2021-10-11 18:50:05 -0700

    created method for first contact and all contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,11 @@
 class ContactsController < ApplicationController
   
   def first_contact
+    contact1 = Contact.first
+    render json: {first_contact: contact1}
+  end
+
+  def all_contacts
     contacts = Contact.all
     render json: {contacts: contacts}
   end
